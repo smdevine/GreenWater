95588c5e4a935f84736e3ceaf28d3190013dc0e5
smdevine <smdevine@ucdavis.edu>
2017-06-15 15:31:06 -0700

    Further modified script for downloading spatial CIMIS data and producing relative humidity data

--- GetData/spatialCIMIS.R
+++ GetData/spatialCIMIS.R
@@ -5,32 +5,38 @@ library(XML)
 library(httr)
 #NOTE: 1/1/2003-2/19/2003, 3/7/2003-3/9/2003, 4/9/2003-4/10/2003, 6/15/2003, 6/17/2003, 6/19/2003-6/22/2003, 6/24/2003, and a number more dates before 9/30/2003 are missing. 2004 water year to the present is good to go.
 californiaDir <- 'C:/Users/smdevine/Desktop/SpatialData/CA_counties/government_units'
-spatialCIMISdir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/SpatialCIMIS'
 cellsofinterestDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/results/model_scaffold'
+#below is specific to downloading data
 ca_ta <- showP4(showWKT("+init=epsg:3310"))
-startyear <- '2003'
+spatialCIMISdir <- 'D:/SpatialCIMIS'
+#'C:/Users/smdevine/Desktop/Allowable_Depletion/SpatialCIMIS'
+startyear <- '2017'
 endyear <- '2017'
-startdate <- strptime(paste0("10/1/", startyear), '%m/%d/%Y')
-enddate <- strptime(paste0("06/01/", endyear), '%m/%d/%Y')
+startdate <- strptime(paste0("06/02/", startyear), '%m/%d/%Y')
+enddate <- strptime(paste0("06/13/", endyear), '%m/%d/%Y')
 datesequence <- seq.Date(from=as.Date(startdate), to=as.Date(enddate), by='day')
 base_url <- 'http://cimis.casil.ucdavis.edu/cimis/'
-varofinterest <- 'ETO.asc.gz'
+varofinterest <- 'ETo.asc.gz'
 varDir <- 'ETo'
-i <- 1
+#i <- 1
 for (i in 1:length(datesequence)) {
   day <- format.Date(datesequence[i], '%d')
   mnth <- format.Date(datesequence[i], '%m')
   yr <- format.Date(datesequence[i], '%Y')
-  if (file.exists(file.path(spatialCIMISdir, yr)) == FALSE) {
-    dir.create(file.path(spatialCIMISdir, yr))
+  if (file.exists(file.path(spatialCIMISdir, varDir)) == FALSE) {
+    dir.create(file.path(spatialCIMISdir, varDir))
   }
-  url_download <- paste0(base_url, yr, '/', mnth, '/', day, '/', 'ETo.asc.gz')
+  if (file.exists(file.path(spatialCIMISdir, varDir, yr)) == FALSE) {
+    dir.create(file.path(spatialCIMISdir, varDir, yr))
+  }
+  url_download <- paste0(base_url, yr, '/', mnth, '/', day, '/', varofinterest)
   setwd(file.path(spatialCIMISdir, varDir, yr))
   error_catch <- try(download.file(url_download, destfile = varofinterest, quiet = TRUE, mode = 'wb'))
   if (class(error_catch)=='try-error') {
+    print(error_catch)
     next
   }
-  cimis_table <- read.table("ETo.asc.gz", skip=6, na.strings="-9999")
+  cimis_table <- read.table(varofinterest, skip=6, na.strings="-9999")
 #this two step operation is much faster than the "for loop" solution
   cimis_table <- t(cimis_table)
   cimis_values <- as.vector(cimis_table)
@@ -52,20 +58,26 @@ list.files()
 california <- shapefile("california_CA_TA.shp")
 plot(california, add=T)
 
-#as noted in first 6 rows of table of raw data in "ETO.asc.gz"
-#              V1      V2
-#1           ncols     510
-#2           nrows     560
-#3       xllcorner -410000
-#4       yllcorner -660000
-#5        cellsize    2000
-#6    NODATA_value   -9999
-
-showP4(showWKT("+init=epsg:3310"))
-showEPSG(proj4string(california)) #can't find it this way
-proj4string(cimis_raster) #NAD83 datum is implied by 
-projInfo(type='datum')
-
+#calculate relative humidity
+startyear <- '2003'
+endyear <- '2017'
+startdate <- strptime(paste0("10/01/", startyear), '%m/%d/%Y')
+enddate <- strptime(paste0("06/13/", endyear), '%m/%d/%Y')
+datesequence <- seq.Date(from=as.Date(startdate), to=as.Date(enddate), by='day')
+i <- 1
+for (i in 1:length(datesequence)) {
+  day <- format.Date(datesequence[i], '%d')
+  mnth <- format.Date(datesequence[i], '%m')
+  yr <- format.Date(datesequence[i], '%Y')
+  setwd(file.path(spatialCIMISdir, 'Tmin', yr))
+  Tmin <- raster(paste0('Tmin', yr, mnth, day, '.tif'))
+  setwd(file.path(spatialCIMISdir, 'Tmax', yr))
+  Tmax <- raster(paste0('Tmax', yr, mnth, day, '.tif'))
+  setwd(file.path(spatialCIMISdir, 'Tdew', yr))
+  Tdew <- raster(paste0('Tdew', yr, mnth, day, '.tif'))
+  Ea <- calc(Tdew, fun = function(x){0.6108*exp(17.27*x/(x+237.3))})
+  Es <- overlay(x=Tmin, y=Tmax, fun=function(x,y){0.6108/2*(exp(17.27*x/(x+237.3))+exp(17.27*y/(y+237.3)))})
+  RH <- overlay(x=Ea, y=Es, fun=function(x,y){100*x/y})
 #get SpatialCIMIS into matrix for cells of interest
 setwd(cellsofinterestDir)
 list.files()
@@ -125,3 +137,17 @@ file.remove(fname)
 #'Tn.asc.gz' is -36.2 - 18.3 scale for 1/1/2016 #daily minimum air temperature at 1.5m
 #'Tx.asc.gz' is -15.8 - 22.8 scale for 1/1/2016 #daily maximum air temperature at 1.5m
 #'U2.asc.gz" is 0 - 3.46 m/s #wind speed at 2m
+
+#as noted in first 6 rows of table of raw data in "ETO.asc.gz"
+#              V1      V2
+#1           ncols     510
+#2           nrows     560
+#3       xllcorner -410000
+#4       yllcorner -660000
+#5        cellsize    2000
+#6    NODATA_value   -9999
+
+showP4(showWKT("+init=epsg:3310"))
+showEPSG(proj4string(california)) #can't find it this way
+proj4string(cimis_raster) #NAD83 datum is implied by 
+projInfo(type='datum')
