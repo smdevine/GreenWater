88e550045d9489ad195b6784f338772a66412549
smdevine <smdevine@ucdavis.edu>
2017-06-28 22:28:30 -0700

    Further model development 6/28/17.  Pushing forward on loop concept

--- ModelConstruction/FAO56_dualcropcoeff.R
+++ ModelConstruction/FAO56_dualcropcoeff.R
@@ -3,7 +3,7 @@ modelscaffoldDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/results/model
 setwd(modelscaffoldDir)
 irrigation_parameters <- read.csv('irrigation_parameters.csv', stringsAsFactors = F)
 crop_parameters <- read.csv('crop_parameters.csv', stringsAsFactors = F) #necessary parameters by crop to run the FAO56 dual crop coefficient ET model
-PRISMprecip <- read.csv('PRISM_precip_data.csv') #this is a daily summary of precip from 10/1/2003-6/25/17 from 'free' daily PRISM 4km resolution for cells of interest in California, created in download_PRISM.R script (from 6/26/17 download); blanks checked for in 'data_QA_QC.R'
+PRISMprecip <- read.csv('PRISM_precip_data.csv', stringsAsFactors = F) #this is a daily summary of precip from 10/1/2003-6/25/17 from 'free' daily PRISM 4km resolution for cells of interest in California, created in download_PRISM.R script (from 6/26/17 download); blanks checked for in 'data_QA_QC.R'
 U2 <- read.csv('SpatialCIMIS_U2_rounded.csv', stringsAsFactors = F) #this is a daily summary of wind data from download of spatial CIMIS data, created in spatialCIMIS.R script.  No missing data except for cell 148533
 RHmin <- read.csv('SpatialCIMIS_minRH_rounded_QCpass.csv', stringsAsFactors = F) #this is a daily summary of minimum relative humidity, estimated from download of spatial CIMIS Tdew and Tmax data, created in spatialCIMIS.R script.  Blanks filled on "12_08_2011" in data_QA_QC.R.  Now, no missing data except for cell 148533
 ETo <- read.csv('SpatialCIMIS_ETo_rounded_QCpass.csv', stringsAsFactors = F) #this is a daily summary of reference ET from download of spatial CIMIS data, created in spatialCIMIS.R script.  Blanks filled on multiple days in data_QA_QC.R.  Now, no missing data except for cell 148533
@@ -14,7 +14,10 @@ if (length(U2$DOY)==length(RHmin$DOY) & length(U2$DOY)==length(ETo$DOY)){
   print('Temporal coverages match in Spatial CIMIS.')
 } else { print('There are differing temporal coverages in the Spatial CIMIS data.')
 }
-#
+#get precip and spatial CIMIS data to same temporal end point
+last_date <- ETo$dates[nrow(ETo)]
+PRISMprecip <- PRISMprecip[1:which(PRISMprecip$dates==last_date), ]
+
 #trim the PRISM data to the Spatial CIMIS temporal coverage
 
 crop_parameters_define <- function(crop_parameters) {
@@ -26,13 +29,6 @@ crop_parameters_define <- function(crop_parameters) {
   return(crop_parameters)
 }
 crop_parameters <- crop_parameters_define(crop_parameters)
-
-ETc_act <- function(Kc_act, ETo) {#equation 3 in Allen et al. 2005
-  Kc_act*ETo
-} 
-Kc_act <- function(Ks, Kcb, Ke) {#equation 4 in Allen et al. 2005
-  Ks*Kcb+Ke
-} 
 
 Kcb <- function(doy, parameters, crop) { #find standard value of Kcb by day of year relative to three reference Kcb points defined by crop parameters
   parameters <- crop_parameters[which(crop_parameters$crop==crop), ]
@@ -69,16 +65,19 @@ plot(fc_alldays, type='l')
 #TO-DO: implement alternative fc calculation in accordance with Eq. 11 from Allen et al. 2005: ((Kcb-Kcmin)/(Kcmax-Kcmin))^(1+0.5*h).  However, this produced a strange result in spreadsheet model for almonds, where increasing h decreases fc.
 
 #calculate fraction of soil wetted by both irrigation and precipitation and exposed to rapid drying (fewi) and fraction of soil exposed to rapid drying and is wetted by precipitation only (fewp).  These are dependent upon fraction of cover calculation above
-fewi_calc <- function(fc, irr_parameters, irr_type) {
-  fw <- irr_parameters$fw[which(irr_parameters$irrigation_type==irr_type)]
+fw_select <- function(irr_parameters, irr_type) {
+  irr_parameters$fw[which(irr_parameters$irrigation_type==irr_type)]
+}
+fw <- fw_select(irrigation_parameters, "Microspray, orchards")
+fewi_calc <- function(fc, fw) { #see p.147 of FAO-56 Chp.7 re: drip
   fewi_temp <- pmin(1-fc,fw)
   fewi_temp[which(fewi_temp<0.01)] <- 0.01 #lower limit on fewi for numeric stability
   fewi_temp[which(fewi_temp>1)] <- 1 #upper limit on fewi for numeric stability
   return(fewi_temp)
 }
 print(irrigation_parameters$irrigation_type)
-fewi_alldays <- fewi_calc(fc_alldays, irrigation_parameters, "Microspray, orchards")
-plot(fewi_alldays, type='l')
+fewi <- fewi_calc(fc_alldays, fw)
+plot(fewi, type='l')
 
 fewp_calc <- function(fc, fewi) {
   fewp_temp <- 1-fc-fewi
@@ -86,16 +85,140 @@ fewp_calc <- function(fc, fewi) {
   fewp_temp[which(fewp_temp>1)] <- 1 #upper limit on fewp for numeric stability
   return(fewp_temp)
 }
-fewp_alldays <- fewp_calc(fc_alldays, fewi_alldays)
-class(fewp_alldays)
-plot(fewp_alldays, type='l')
-plot(fewp_alldays+fewi_alldays, type='l')
-summary(fewp_alldays+fewi_alldays)
+fewp <- fewp_calc(fc_alldays, fewi_alldays)
+plot(fewp, type='l')
+plot(fewp + fewi, type='l')
+summary(fewp + fewi)
 
 #calculate Kri and Krp; now dependent on daily soil water balance
 #Kri=(TEW-De,j-1)/(TEW-REW) (eqn. 22)
 #Krp=(TEW-Dep,j-1)/(TEW-REW) (eqn. 23)
+#this is example for model code 233269: PRISM cell=365380; SpatialCIMIS cell=86833; SSURGO cokey=12753827; crop=almonds
+i <- 1
+model.length <- nrow(ETo)
+Dei.initial <- numeric(length = model.length)
+Kri <- numeric(length = model.length)
+Kei <- numeric(length = model.length)
+Ei <- numeric(length = model.length)
+DPei <- numeric(length = model.length)
+Dei.end <- numeric(length = model.length)
+Dep.initial <- numeric(length = model.length)
+Krp <- numeric(length = model.length)
+Kep <- numeric(length = model.length)
+Ep <- numeric(length = model.length)
+DPep <- numeric(length = model.length)
+Dep.end <- numeric(length = model.length)
+W <- numeric(length = model.length)
+Kc.ns <- numeric(length = model.length)
+ETc.ns <- numeric(length = model.length)
+Dr.initial <- numeric(length = model.length)
+Ir <- numeric(length = model.length)
+DPr <- numeric(length = model.length)
+Ks <- numeric(length = model.length)
+Kc.adj <- numeric(length = model.length)
+Dr.end <- numeric(length = model.length)
+ETc.adj <- numeric(length = model.length)
+P <- PRISMprecip[ ,which(colnames(PRISMprecip)=='cell_365380')] #this can be automated based on the model_scaffold.csv that defines all the spatial parameters
+ETo.model <- ETo[ ,which(colnames(ETo)=='cell_86833')]
+REW.parameter <- 8
+TEW_calc <- function(ThetaFC, ThetaWP, Ze, REW) { #Ze is depth of upper soil layer where evaporation occurs in meters
+  result <- 1000 * (ThetaFC - 0.5 * ThetaWP) * Ze
+  if (result < REW) {
+    stop(print('Hay una problema con TEW')) #could change to next and print or save model code
+  } 
+  return(result)
+}
+TEW.parameter <- TEW(0.336, 0.211, 0.1, REW.parameter)
+# irrig.prevday <- function(irrig, i) {
+#   if (i==1) {
+#     0
+#   } else {
+#     irrig[i-1]
+#   }
+# }
+Dep.initial.calc <- function(TEW, TEW_fraction, Dep, P, i) {
+  if (i==1) {
+    TEW*TEW_fraction #this is an initial estimate of the water balance for the exposed surface soil where irrigation is not applied that assumes all daily precip occurs early in the morning so as to estimate Ke and Kr
+  } else {
+    max(Dep[i-1] - P[i], 0)
+  }
+}
+Dep.initial[i] <- Dep.initial.calc(TEW.parameter, 0.5, Dep, P, i)
+Dei.initial.calc <- function(TEW, TEW_fraction, Dei, P, Ir, fw, i) {
+  if (i==1) {
+    TEW*TEW_fraction
+  } else {
+    max(Dei[i-1] - P[i] - Ir[i-1] / fw, 0) #have to use irrigation from previous day because current day irrigation decision is dependent on this calc
+  }
+}
+Dei.initial[i] <- Dei.initial.calc(TEW.parameter, 0.5, Dei, P, Ir, fw, i)
+Kr_calc <- function(TEW, REW, De.initial) { #can be used to calculate Kri or Krp
+  max(if(De.initial < REW) { #could initialize this in vector above
+    1
+  } else {
+    (TEW - De.initial) / (TEW - REW)
+  } , 0)
+}
+#test Krp calc
+Kri[i] <- Kr_calc(TEW_parameter, REW_parameter, Dep.initial.calc(TEW.parameter, 0.5, Dep, P, i))
+#test Kri calc
+Krp[i] <- Kr_calc(TEW_parameter, REW_parameter, Dei.initial.calc(TEW.parameter, 0.5, Dei, P, Ir, fw, i))
+W_calc <- function(TEW, Dei, Dep, fewp, fewi, i) {
+  1 / (1 + (fewp[i] / fewi[i]) * (TEW - Dep[i]) / (TEW - Dei[i]))
+}
+W[i] <- W_calc(TEW_parameter, Dei, Dep, fewp, fewi, i)
+Kei_calc <- function(Kri, W, Kcmax, Kcb, fewi, i) {
+  min(Kri[i] * W[i] * (Kcmax[i] - Kcb[i]), fewi[i] * Kcmax[i])
+}
+Kei[i] <- Kei_calc(Kri, W, Kcb_df$Kc_max, Kcb_df$Kcb_climate_adj, fewi, i)
+Kep_calc <- function(Krp, W, Kcmax, Kcb, fewp, i) {
+  min(Krp[i] * (1 - W[i]) * (Kcmax[i]- Kcb[i]), fewp[i] * Kcmax[i])
+}
+Kep[i] <- Kep_calc(Krp, W, Kcb_df$Kc_max, Kcb_df$Kcb_climate_adj, fewp, i)
+Ep_calc <- function(ETo, Kep, i) {
+  ETo[i] * Kep[i]
+}
+Ei_calc <- function(ETo, Kei, i) {
+  ETo[i] * Kei[i]
+}
+Ep[i] <- Ep_calc(ETo.model, Kep, i)
+Ei[i] <- Ei_calc(ETo.model, Kei, i)
+DPep_calc <- function(P, Dep.initial, i) {
+  max(P[i] - Dep.initial[i], 0)
+}
+DPep[i] <- DPep_calc(P, Dep.initial, i)
+Dep.end.calc <- function(Dep.initial, P, Ep, fewp, DPep, i) {
+  Dep.initial[i] - P[i] + Ep[i] / fewp[i] + DPep[i]
+}
+Dep.end[i] <- Dep.end.calc(Dep.initial, P, Ep, fewp, DPep, i)
+#now calculate irrigation needs to be able to finish water balance for fewi portion of uper layer
+DPei.calc <- function(P, Ir, fw, Dei.initial, i) {
+  max(if (i==1) {
+    P[i] - Dei.initial[i]
+  } else {
+    P[i] + Ir[i-1] / fw - Dei.initial[i]
+  }, 0)
+}
+DPei[i] <- DPei.calc(P, Ir, fw, Dei.initial, i)
+Kc.ns.calc <- function(Kcb, Kei, Kep, i) {
+  Kcb[i] + Kei[i] + Kep[i]
+}
+Kc.ns[i] <- Kc.ns.calc(Kcb_df$Kcb_climate_adj, Kei, Kep, i)
+ETc.ns.calc <- function(Kc.ns, ETo, i) {
+  Kc.ns[i] * ETo[i]
+}
+ETc.ns[i] <- ETc.ns.calc(Kc.ns, ETo.model, i)
+#Dr.initial is next
 
+Kc.act.calc <- function(Ks, Kcb, Ke) {#equation 4 in Allen et al. 2005
+  Ks[i] * Kcb+Ke
+}
+ETc.act.calc <- function(Kc_act, ETo) {#equation 3 in Allen et al. 2005
+  Kc_act*ETo
+}
+
+
+#De,j=De,j-1 - P,j - Ij/fw + Ej/fewi + DPei,j (again, ignoring tranpiration from upper 10 cm and runoff, eqn. 21) 
 #pseudo-code outline of 'separate prediction of evaporation from soil wetted by precipitation only' following Allen et al. 2005, except ignoring runoff, essentially assuming that runoff will really only occur when soils are near field capacity, so partitioning this as 'deep percolation' is acceptable and is consciously preferred over introduced errors from the curve number approach
 #Ke = Kei + Kep (eqn. 14)
 #where Kei=evaporation coefficient for the exposed fraction of the soil wetted by both irrigation and by precipitation and Kep=evaporation coefficient for the exposed fraction of the soil wetted by precipitation only
