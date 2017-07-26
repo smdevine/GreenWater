a3abf48142dea4556a42deb1fe0a23ae6d1f5cc4
smdevine <smdevine@ucdavis.edu>
2017-07-26 16:44:37 -0700

    modified script to handle newly aggregated SSURGO data

--- ModelConstruction/model_construction.R
+++ ModelConstruction/model_construction.R
@@ -1,20 +1,20 @@
 library(raster)
 #library(rgdal)
 #options(digits = 10, scipen = 999)
-options(stringsAsFactors = F)
+options(stringsAsFactors = FALSE)
 #rasterOptions(progress = 'window')
 mainDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion'
 cropscape <- file.path(mainDir, 'CA_cropscape2015')
 soildataDir <- file.path(mainDir, 'soils_data')
 mu_dir <- file.path(mainDir, 'soils_data/spatial')
-mu_data_dir <- file.path(mainDir, 'soils_data/results/paw_check')
+comp_data_dir <- file.path(mainDir, 'soils_data/results/summer2017model/CA_all')
 results <- file.path(mainDir, 'results')
 cropscape_results <- file.path(results, 'rasters/cropscape')
 soil_results <- file.path(results, 'rasters/soils')
 californiaDir <- 'C:/Users/smdevine/Desktop/SpatialData/CA_counties/government_units'
 spatialCIMIS <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/SpatialCIMIS'
 PRISMDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/PRISMdaily'
-model_scaffoldDir <- file.path(results, 'model_scaffold')
+model_scaffoldDir <- file.path(mainDir, 'model_scaffold')
 model_results <- file.path(results, 'model_results')
 
 #define crop codes
@@ -33,9 +33,9 @@ model_scaffold <- read.csv("model_scaffold_codes_nocoords_6.9.17.csv")
 #lapply(model_scaffold, class)
 #length(unique(model_scaffold$mukey)) #5,225 unique mukeys
 #get number of cokeys per mukey (can rewrite as function to end up with n set of model matrices for each scenario of allowable depletion assumptions and rooting depth)
-setwd(mu_data_dir)
+setwd(comp_data_dir)
 #list.files()
-soil_comp_data <- read.csv("comps_all_final_summary.csv")
+soil_comp_data <- read.csv("CA_all_comps_summary_dbmodified_FINAL2017-07-26.csv")
 #head(soil_comp_data)
 compkeys_n <- function(x) {length(unique(x))}
 soilcomps_n <- as.data.frame(tapply(soil_comp_data$cokey, soil_comp_data$mukey, compkeys_n))
@@ -55,8 +55,56 @@ maxcomps <- max(model_scaffold2$n_compkeys)
 #summary(model_scaffold2$n_compkeys)
 #head(model_scaffold2)
 soil_comp_data <- soil_comp_data[order(soil_comp_data$mukey, soil_comp_data$comppct_r, soil_comp_data$cokey, decreasing=c(F, T, F)), ] #this works as confirmed by writing to csv below
+colnames(soil_comp_data)
+soil_comp_data <- soil_comp_data[ ,c(1:5, 22:26, 32:39)]
+# missing_mukeys <- model_scaffold2$mukey[!(model_scaffold2$mukey %in% soil_comp_data$mukey)]
+# mukeys <- model_scaffold2$mukey[model_scaffold2$mukey %in% soil_comp_data$mukey]
 #setwd(model_scaffoldDir)
 #write.csv(soil_comp_data, 'soil_comp_data_sorted.csv', row.names=F)
+for (i in seq_len(maxcomps)) {
+  soilcomp_rownums <- match(model_scaffold2$mukey, soil_comp_data$mukey)
+  temp <- soil_comp_data[soilcomp_rownums, ]
+  temp$mukey <- NULL
+  temp2 <- cbind(model_scaffold2, temp)
+  setwd(file.path(model_scaffoldDir, 'soil_climate_crop/by_component'))
+  write.csv(temp2, paste0('model_scaffold_comp', as.character(i), '.csv'), row.names=F) #save the file for modeling purposes later
+  model_scaffold2 <- model_scaffold2[-which(model_scaffold2$n_compkeys==i), ] #now get rid of model codes with i number of cokeys.  they don't need to be included in additional model scaffolds
+  soilcomp_rownums <- unique(soilcomp_rownums)
+  soil_comp_data <- soil_comp_data[-soilcomp_rownums, ] #get rid of the cokeys already covered
+}
+
+setwd(file.path(model_scaffoldDir, 'soil_climate_crop/by_component'))
+fnames <- list.files(pattern = glob2rx('*csv'))
+master.file <- do.call(rbind, lapply(fnames, read.csv))
+dim(master.file)
+#1,177,027 unique soil components, climate, and crop
+j <- which(master.file$comppct_r >= 15)
+length(j) #but only 277,477 are major components
+sum(master.file$majcompflag=='Yes')
+model_scaffold_majcomps_almonds <- master.file[master.file$comppct_r >= 15 & master.file$crop_code==almond_code,]
+dim(model_scaffold_majcomps_almonds)
+setwd(file.path(model_scaffoldDir, 'run_model/July2017'))
+write.csv(model_scaffold_majcomps_almonds, 'model_scaffold_majcomps_almonds.csv', row.names = F)
+model_scaffold_majcomps <- master.file[master.file$comppct_r >= 15, ]
+dim(model_scaffold_majcomps)
+setwd(file.path(model_scaffoldDir, 'run_model/July2017'))
+write.csv(model_scaffold_majcomps_almonds, 'model_scaffold_majcomps.csv', row.names = F)
+#investigate NAs and 0's
+mukey_AD_isNA <- unique(model_scaffold2$mukey[which(is.na(model_scaffold2$allowable_depletion))])
+cokey_AD_isNA <- unique(model_scaffold2$cokey[which(is.na(model_scaffold2$allowable_depletion))])
+compnames_NA <- soil_comp_data$compname[match(cokey_AD_isNA, soil_comp_data$cokey)] #confirmed that these are all components for which we shouldn't expect any data
+setwd(soildataDir)
+list.files()
+mu_area <- read.csv('farmland_mu_area.csv')
+mu_area_rows <- match(mukey_AD_isNA, mu_area$mukey)
+mu_area <- mu_area[mu_area_rows,]
+sum(mu_area$hectares)
+#the total ag area of these mukeys are 70,641 hectares; however the raster cells with these should be less, because CropScape really shouldn't be identifying crops in these areas
+tapply(model_scaffold2$allowable_depletion, model_scaffold2$crop_code, mean, na.rm=TRUE)
+model_scaffold2$n_compkeys[which(model_scaffold2$mukey=='455489')]
+
+
+#old version of this function before revised SSURGO July 2017 aggregation work
 for (i in 1:maxcomps) {
   soilcomp_rownums <- match(model_scaffold2$mukey, soil_comp_data$mukey)
   model_scaffold2$cokey_model <- soil_comp_data$cokey[soilcomp_rownums]
@@ -81,34 +129,3 @@ for (i in 1:maxcomps) {
   soilcomp_rownums <- unique(soilcomp_rownums)
   soil_comp_data <- soil_comp_data[-soilcomp_rownums, ] #get rid of the cokeys already covered
 }
-
-summary(model_scaffold2$allowable_depletion)
-setwd(file.path(model_scaffoldDir, 'paw_check/mean_root_depth'))
-fnames <- list.files(pattern = glob2rx('*csv'))
-for (i in 1:length(fnames)) {
-  if (i==1) {
-    master.file <- read.csv(fnames[i])
-  } else {
-    master.file <- rbind(master.file, read.csv(fnames[i]))
-  }
-}
-#1,177,027 unique soil components, climate, and crop
-j <- which(master.file$comppct_r >= 15)
-length(j) #but only 277,477 are major components
-model_scaffold_majcomps <- master.file[j,]
-setwd(model_scaffoldDir)
-write.csv(model_scaffold_majcomps, 'model_scaffold_majcomps.csv', row.names = F)
-
-#investigate NAs and 0's
-mukey_AD_isNA <- unique(model_scaffold2$mukey[which(is.na(model_scaffold2$allowable_depletion))])
-cokey_AD_isNA <- unique(model_scaffold2$cokey[which(is.na(model_scaffold2$allowable_depletion))])
-compnames_NA <- soil_comp_data$compname[match(cokey_AD_isNA, soil_comp_data$cokey)] #confirmed that these are all components for which we shouldn't expect any data
-setwd(soildataDir)
-list.files()
-mu_area <- read.csv('farmland_mu_area.csv')
-mu_area_rows <- match(mukey_AD_isNA, mu_area$mukey)
-mu_area <- mu_area[mu_area_rows,]
-sum(mu_area$hectares)
-#the total ag area of these mukeys are 70,641 hectares; however the raster cells with these should be less, because CropScape really shouldn't be identifying crops in these areas
-tapply(model_scaffold2$allowable_depletion, model_scaffold2$crop_code, mean, na.rm=TRUE)
-model_scaffold2$n_compkeys[which(model_scaffold2$mukey=='455489')]
