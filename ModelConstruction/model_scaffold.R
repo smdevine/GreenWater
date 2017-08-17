f17ba224d652481dfd9f53547c6ef714f379a704
smdevine <smdevine@ucdavis.edu>
2017-08-17 12:47:26 -0700

    work to add additional crops to model scaffold

--- ModelConstruction/model_scaffold.R
+++ ModelConstruction/model_scaffold.R
@@ -1,3 +1,5 @@
+#on 8/18, merged relevant scrip from 'merge_cropscape_soils_data.R' so model_scaffold
+#can work as a standalone to set up the model parameter matrix
 library(raster)
 library(rgdal)
 options(digits = 22, scipen = 999)
@@ -14,11 +16,62 @@ spatialCIMIS <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/SpatialCIMIS'
 PRISMDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/PRISMdaily'
 ###this follows some raster processing in merge_cropscape_soils_data.R
 ###purpose of this script is to determine number of unique soil and crop scenarios and construct a model matrix that will be more memory efficient than a raster based model, since the majority of the cells in the raster are not of interest
+setwd(mu_dir)
+mu_raster <- raster('farmland_mu_albers.tif')
+#split soils mu raster into 4 tiles before further processing
+if (!dir.exists(file.path(soil_results, 'main_tiles'))) {
+  dir.create(file.path(soil_results, 'main_tiles'))
+}
+splitRaster(mu_raster, 2, 2, path=file.path(soil_results, 'main_tiles')) #these
+#files were subsequently deleted after converting to tif
+
+#save soils mu raster tiles as tif files to save memory
+
+setwd(file.path(soil_results, 'main_tiles'))
+tiles <- list.files(pattern = glob2rx('*.grd'))
+for (i in 1:4) {
+  tile <- raster(tiles[i])
+  writeRaster(tile, paste0('farmland_mu_albers_tile', as.character(i), '.tif'), format='GTiff')
+}
+
+#isolate crops of interest
+setwd(cropscape)
+cropscape_raster <- raster('cropscape_masked_ag.tif')
+cropscape_legend <- read.csv('cropscape_legend.txt', stringsAsFactors = FALSE)
+alfalfa <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Alfalfa']
+grapes <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Grapes']
+almonds <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Almonds']
+walnuts <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Walnuts']
+pistachios <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Pistachios']
+tomatoes <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Tomatoes']
+wheat <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Winter Wheat']
+
+#split cropscape raster into four tiles and then select crops of interest for masking soil rasters
+splitRaster(cropscape_raster, 2, 2, path=file.path(cropscape_results, 'main_tiles'))
+setwd(file.path(cropscape_results, 'main_tiles'))
+tiles <- list.files(file.path(cropscape_results, 'main_tiles'), pattern = glob2rx('*.grd'))
+crops <- c('alfalfa', 'grapes', 'almonds', 'walnuts', 'pistachios', 'tomatoes', 'wheat')
+crop_codes <- c(alfalfa, grapes, almonds, walnuts, pistachios, tomatoes, wheat)
+
+for (i in 1:4) { #refers to 4 tiles
+  setwd(file.path(cropscape_results, 'main_tiles'))
+  study.crops.raster <- raster(tiles[i])
+  study.crops.raster[!(study.crops.raster$cropscape_masked_ag %in% crop_codes)] <- NA
+  if (!dir.exists(file.path(cropscape_results, 'study_crops'))) {
+    dir.create(file.path(cropscape_results, 'study_crops'))
+  }
+  setwd(file.path(cropscape_results, 'study_crops'))
+  writeRaster(study.crops.raster, paste0('study_crops_tile', as.character(i), '.tif'), format='GTiff')
+}
+
 #first part saves new mukey rasters that are masked by crops of interest
 setwd(file.path(cropscape_results, 'study_crops')) #replace 1 with j
 crop_tiles <- list.files(pattern = glob2rx('*.tif'))
 setwd(file.path(soil_results, 'main_tiles')) 
 soil_tiles <- list.files(pattern = glob2rx('*.tif'))
+if (!dir.exists(file.path(soil_results, 'study_crops'))) {
+  dir.create(file.path(soil_results, 'study_crops'))
+}
 for (i in 1:4) {
   setwd(file.path(cropscape_results, 'study_crops'))
   crop_tile <- raster(crop_tiles[i]) #change back to i
@@ -31,37 +84,39 @@ for (i in 1:4) {
 }
 setwd(file.path(soil_results, 'study_crops'))
 soil_cropmasked_tiles <- list.files(pattern = glob2rx('*.tif'))
+
 #check acreages of these tiles to confirm that they match crop acreages
-soil_tile <- raster(soil_cropmasked_tiles[i])
-soil_temp <- soil_tile
-soil_temp[!is.na(soil_temp$mu_cropmasked_tile3)] <- 1
-cellStats(soil_temp, sum, na.rm=TRUE)
+# soil_tile <- raster(soil_cropmasked_tiles[i])
+# soil_temp <- soil_tile
+# soil_temp[!is.na(soil_temp$mu_cropmasked_tile3)] <- 1
+# cellStats(soil_temp, sum, na.rm=TRUE)
 
 #plot the study crops if so desired; need to work on raster legend
-setwd(file.path(cropscape_results, 'study_crops'))
-crop_tiles <- list.files(pattern = glob2rx('*.tif'))
-crop_tile1 <- raster(crop_tiles[1])
-extent1 <- extent(crop_tile1)
-crop_tile2 <- raster(crop_tiles[2])
-extent2 <- extent(crop_tile2)
-crop_tile3 <- raster(crop_tiles[3])
-extent3 <- extent(crop_tile3)
-crop_tile4 <- raster(crop_tiles[4])
-extent4 <- extent(crop_tile4)
-setwd(californiaDir)
-california <- shapefile("california_CA_TA.shp")
-california_aea <- spTransform(california, crs(crop_tile1))
-plot(california_aea)
-plot(crop_tile1, add=T, legend=F)
-plot(crop_tile2, add=T, legend=F)
-plot(crop_tile3, add=T, legend=F)
-plot(crop_tile4, add=T, legend=F)
-plot(extent1, add=T)
-plot(extent2, add=T)
-plot(extent3, add=T)
-plot(extent4, add=T)
+# setwd(file.path(cropscape_results, 'study_crops'))
+# crop_tiles <- list.files(pattern = glob2rx('*.tif'))
+# crop_tile1 <- raster(crop_tiles[1])
+# extent1 <- extent(crop_tile1)
+# crop_tile2 <- raster(crop_tiles[2])
+# extent2 <- extent(crop_tile2)
+# crop_tile3 <- raster(crop_tiles[3])
+# extent3 <- extent(crop_tile3)
+# crop_tile4 <- raster(crop_tiles[4])
+# extent4 <- extent(crop_tile4)
+
+# california_aea <- spTransform(california, crs(crop_tile1))
+# plot(california_aea)
+# plot(crop_tile1, add=T, legend=F)
+# plot(crop_tile2, add=T, legend=F)
+# plot(crop_tile3, add=T, legend=F)
+# plot(crop_tile4, add=T, legend=F)
+# plot(extent1, add=T)
+# plot(extent2, add=T)
+# plot(extent3, add=T)
+# plot(extent4, add=T)
 
 #create a spatial points dataframe out of the raster cells of interest with mukeys, cropcodes, and coordinates
+setwd(californiaDir)
+california <- shapefile("california_CA_TA.shp")
 setwd(file.path(soil_results, 'study_crops'))
 soil_cropmasked_tiles <- list.files(pattern = glob2rx('*.tif'))
 setwd(file.path(cropscape_results, 'study_crops'))
@@ -94,47 +149,52 @@ lapply(results_df, class) #get the classes of each df column
 results_df$mukey_cropcode <- as.numeric(paste0(results_df$mukey, results_df$crop_code))
 nrow(results_df)
 #17,220,605 rows in the data.frame (these are the number of 30 x 30 m raster cells of interest)
+#now, 21,679,100
 length(unique(results_df$mukey))
 #5,225 unique mukeys
+#now, 5,620
 length(unique(results_df$mukey_cropcode))
 #13,309 unique crop x mukey combinations
+#now, 20,605
 mukeys_cropcodes_AEA <- results_df
 coordinates(mukeys_cropcodes_AEA) <- c("longitude_AEA", "latitude_AEA")
 proj4string(mukeys_cropcodes_AEA) <- '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs' #this is Albers Equal Area coordinates
-results_CA_TA <- spTransform(results_df, crs(california)) #California Teale Albers coordinates for getting SpatialCimis cell numbers
+results_CA_TA <- spTransform(mukeys_cropcodes_AEA, crs(california)) #California Teale Albers coordinates for getting SpatialCimis cell numbers
 results_df_CA_TA <- as.data.frame(results_CA_TA)
 head(results_df_CA_TA)
-colnames(results_df_CA_TA)[3:4] <- c('longitude_CA_TA', 'latitude_CA_TA')
-setwd('C:/Users/smdevine/Desktop/Allowable_Depletion/results/data.frames')
+colnames(results_df_CA_TA)[4:5] <- c('longitude_CA_TA', 'latitude_CA_TA')
+setwd(file.path(results, 'data.frames'))
 write.csv(results_df_CA_TA, 'mukeys_cropcodes_CA_TA.csv', row.names = FALSE)
 mukeys_cropcodes_longlatNAD83 <- spTransform(mukeys_cropcodes_AEA, "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0") #as defined from a prism raster
+mukeys_cropcodes_longlatNAD83_df <- as.data.frame(mukeys_cropcodes_longlatNAD83)
+colnames(mukeys_cropcodes_longlatNAD83_df)[4:5] <- c('longitude_NAD83', 'latitude_NAD83')
 #setwd('C:/Users/smdevine/Desktop/Allowable_Depletion/results/shapefiles')
 #shapefile(mukeys_cropcodes_longlatNAD83, 'mukeys_cropcodes_longlatNAD83.shp')
 #mukeys_cropcodes_longlatNAD83 <- shapefile('mukeys_cropcodes_longlatNAD83.shp') #this is much slower than reading in the csv file
 setwd(file.path(results, 'data.frames'))
-write.csv(mukeys_cropcodes_longlatNAD83, 'mukeys_cropcodes_longlatNAD83.csv')
+write.csv(mukeys_cropcodes_longlatNAD83_df, 'mukeys_cropcodes_longlatNAD83.csv')
 
 #read in CA_TA projection now and get cell numbers from
 setwd(file.path(results, 'data.frames'))
 mukeys_cropcodes_CA_TA <- read.csv('mukeys_cropcodes_CA_TA.csv')
 coordinates(mukeys_cropcodes_CA_TA) <- c('longitude_CA_TA', 'latitude_CA_TA')
 crs(mukeys_cropcodes_CA_TA) <- '+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0' #this is California Teale Albers, as defined by California state boundary shapefile
-setwd(file.path(spatialCIMIS, '2004'))
-spatialCIMIS <- raster('ETo_20040101.tif')
+setwd(file.path(spatialCIMIS, 'U2/2004'))
+spatialCIMIS <- raster('U220040101.tif')
 CIMIScellnumbers <- as.integer(cellFromXY(object=spatialCIMIS, xy=mukeys_cropcodes_CA_TA))
 summary(CIMIScellnumbers)
-CIMIScellunique <- unique(CIMIScellnumbers) #17,341 CIMIS cells need sampling
-setwd(file.path(results, 'model_scaffold'))
+CIMIScellunique <- unique(CIMIScellnumbers) #17,341 CIMIS cells need sampling; 18,633
+setwd(file.path(mainDir, 'model_scaffold/Aug2017'))
 CIMIScellunique_df <- data.frame(CIMIS_cells=CIMIScellunique)
 write.csv(CIMIScellunique_df, 'CIMIS_cells_unique.csv', row.names = F)
 #get cell numbers from PRISM for each point of interest
-setwd(file.path(PRISMDir, 'PRISM_ppt_stable_4kmD2_20031001_bil'))
+setwd(file.path(PRISMDir, '2003/PRISM_ppt_stable_4kmD2_20031001_bil'))
 prism <- raster('PRISM_ppt_stable_4kmD2_20031001_bil.bil')
 PRISMcellnumbers <- as.integer(cellFromXY(object = prism, xy = mukeys_cropcodes_longlatNAD83))
 summary(PRISMcellnumbers)
-PRISMcellunique <- unique(PRISMcellnumbers) #5338 PRISM cells need sampling
+PRISMcellunique <- unique(PRISMcellnumbers) #5338 PRISM cells need sampling; 5701 need sampling
 PRISMcellunique_df <- data.frame(PRISM_cells=PRISMcellunique) 
-setwd(file.path(results, 'model_scaffold'))
+setwd(file.path(mainDir, 'model_scaffold/Aug2017'))
 write.csv(PRISMcellunique_df, 'PRISM_cells_unique.csv', row.names = F)
 
 #add PRISM and CIMIS raster cell numbers to find unique combinations of soil, crop, and climate
@@ -143,7 +203,7 @@ results_df$PRISMcellnumber <- PRISMcellnumbers
 results_df$CIMIScellnumber <- CIMIScellnumbers
 results_df$model_code <- paste0(results_df$mukey, results_df$crop_code, results_df$PRISMcellnumber, results_df$CIMIScellnumber)
 #results_df$model_code <- as.numeric(results_df$model_code) #set options(digits=22, scipen=999) before writing this to csv.  However, excel will round all number after 15 digits if opening the csv.  DANGER! long integers are being changed somehow, which creates the impression there are less than 200k unique combinations!  conclusion is that these should be kept in character class to preserve integrity
-model_codes <- unique(results_df$model_code) #242,714 unique combinations of soil, crop, and climate out of 17,220,605 where soil is map unit
+model_codes <- unique(results_df$model_code) #242,714 unique combinations of soil, crop, and climate out of 17,220,605 where soil is map unit; now, 341,207 unique combinations
 unique_model_code <- 100001:(100000+length(model_codes)) #create a dummy code for the model matrix
 model_codes_matrix <- data.frame(model_code=model_codes, unique_model_code=unique_model_code)
 final_results_df <- merge(results_df, model_codes_matrix, by='model_code')
@@ -153,15 +213,18 @@ setwd(file.path(results, 'data.frames'))
 write.csv(final_results_df, 'mukeys_cropcodes_climatecodes_AEA.csv', row.names = FALSE)
 cells_to_model <- match(unique_model_code, final_results_df$unique_model_code)
 model_scaffold <- final_results_df[cells_to_model, ]
-model_scaffold$model_code <- NULL
-model_scaffold$full_matrix_rownum <- rownames(model_scaffold)
-model_scaffold$longitude_AEA <- NULL
-model_scaffold$latitude_AEA <- NULL
-setwd(file.path(results, 'model_scaffold'))
-write.csv(model_scaffold, 'model_scaffold_codes_nocoords_6.9.17.csv', row.names = F) #this destroys the model code numbers when read back in, so they were deleted to avoid future confusion. they can simply be re-created by paste0(results_df$mukey, results_df$crop_code, results_df$PRISMcellnumber, results_df$CIMIScellnumber) in this order
-model_scaffold <- read.csv('model_scaffold_codes6.8.17.csv')
 head(model_scaffold)
-lapply(model_scaffold, class)
+model_scaffold$model_code <- NULL
+model_scaffold$mukey_cropcode <- NULL
+#model_scaffold$full_matrix_rownum <- rownames(model_scaffold)
+#model_scaffold$longitude_AEA <- NULL
+#model_scaffold$latitude_AEA <- NULL
+setwd(file.path(mainDir, 'model_scaffold'))
+write.csv(model_scaffold, 'model_scaffold_codes8.17.17.csv', row.names = F) #this destroys the model code numbers when read back in, so they were deleted to avoid future confusion. they can simply be re-created by paste0(results_df$mukey, results_df$crop_code, results_df$PRISMcellnumber, results_df$CIMIScellnumber) in this order
+model_scaffold <- read.csv('model_scaffold_codes8.17.17.csv')
+
+
+#rasterization testing
 setwd(file.path(results, 'data.frames'))
 model_points <- read.csv('mukeys_cropcodes_climatecodes_AEA.csv')
 model_points_sp <- model_points
