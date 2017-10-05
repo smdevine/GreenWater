4853ac5dcccd9d8de0614ffb4681963f891c21b8
smdevine <smdevine@ucdavis.edu>
2017-10-05 10:48:33 -0700

    script for grape re-run

--- ModelConstruction/run_model.R
+++ ModelConstruction/run_model.R
@@ -90,7 +90,30 @@ FAO56DualCropCalc('almond.mature', almond_code, 80, '2.0m', "Microspray, orchard
 #alfalfa.intermountain 1.0 m, 30% AD re-run from scratch
 FAO56DualCropCalc('alfalfa.intermountain', alfalfa_code, 30, '1.0m', "Border", crop.parameters.df, model.scaffold, U2.df, P.df, ETo.df, RHmin.df, results_file='new', row_start=1, RDI.min = NA, alfalfa.zone = 'Intermountain')
 
-#wine grape re-run according to geography (model scaffold had 'grape.zone' added)
+#wine grape test run according to geography (model scaffold had 'grape.zone' added)
 FAO56DualCropCalc('grapes.wine', grape_code, 50, '2.0m', "Drip", crop.parameters.df, model.scaffold, U2.df, P.df, ETo.df, RHmin.df, results_file='new', row_start=1, RDI.min = 0.5, alfalfa.zone = NA, grape.zone = 'Central California Foothills and Coastal Mountains')
 
+#re-run of grape scenarios
+#computer outage required partial re-run on 10/2/17
+modelgrid <- expand.grid(root.depths = c('1.0m', '2.0m', '3.0m'), AD.percentage = c(30, 50, 80), cropnames = c('grapes.wine', 'grapes.table'))
+modelgrid$root.depths <- as.character(modelgrid$root.depths)
+modelgrid$cropnames <- as.character(modelgrid$cropnames)
+modelgrid$AD.percentage <- as.integer(modelgrid$AD.percentage)
+modelgrid$cropcode <- ifelse(modelgrid$cropnames=='almond.mature', almond_code, ifelse(modelgrid$cropnames=='walnut.mature', walnut_code, ifelse(modelgrid$cropnames=='pistachios', pistachio_code, ifelse(modelgrid$cropnames=='grapes.table', grape_code, ifelse(modelgrid$cropnames=='grapes.wine', grape_code, ifelse(modelgrid$cropnames=='alfalfa.intermountain' | modelgrid$cropnames=='alfalfa.CV' | modelgrid$cropnames=='alfalfa.imperial', alfalfa_code, print('Done')))))))
+modelgrid$irrtype <- ifelse(modelgrid$cropnames=='almond.mature', 'Microspray, orchards', ifelse(modelgrid$cropnames=='walnut.mature', 'Microspray, orchards', ifelse(modelgrid$cropnames=='pistachios', 'Microspray, orchards', ifelse(modelgrid$cropnames=='grapes.table', 'Drip', ifelse(modelgrid$cropnames=='grapes.wine', 'Drip', ifelse(modelgrid$cropnames=='alfalfa.intermountain' | modelgrid$cropnames=='alfalfa.CV' | modelgrid$cropnames=='alfalfa.imperial', 'Border', print('Done')))))))
+modelgrid$alfalfa.zone <- ifelse(modelgrid$cropnames=='alfalfa.intermountain', 'Intermountain', ifelse(modelgrid$cropnames=='alfalfa.CV', 'Central Valley', ifelse(modelgrid$cropnames=='alfalfa.imperial', 'Imperial Valley', NA)))
+modelgrid$grape.zone <- ifelse(modelgrid$cropnames=='grapes.wine', 'Central California Foothills and Coastal Mountains', ifelse(modelgrid$cropnames=='grapes.table', 'Central California Valley', NA))
+modelgrid$AD.percentage[modelgrid$cropnames=='grapes.wine'] <- 50
+modelgrid$RDI.min <- c(0.2, 0.2, 0.2, 0.5, 0.5, 0.5, 0.8, 0.8, 0.8, rep(NA, 9))
+modelgrid
+cl <- makeCluster(6, type = 'SOCK') #change the number to your desired number of CPU cores  
+clusterExport(cl, list=c("resultsDir", "rounding_digits", "FAO56DualCropCalc", "crop.parameters.df", "model.scaffold", "U2.df", "P.df", "ETo.df", "RHmin.df", "irrigation.parameters", "cropscape_legend"))
+registerDoSNOW(cl)
+modelgrid <- modelgrid[c(8, 10:18),] #trim the grid for the re-run
+modelgrid
+foreach(i=1:10) %dopar% {
+  FAO56DualCropCalc(modelgrid$cropnames[i], modelgrid$cropcode[i], modelgrid$AD.percentage[i], modelgrid$root.depths[i], modelgrid$irrtype[i], crop.parameters.df, model.scaffold, U2.df, P.df, ETo.df, RHmin.df, results_file = 'new', row_start = 1, RDI.min = modelgrid$RDI.min[i], alfalfa.zone = modelgrid$alfalfa.zone[i], grape.zone = modelgrid$grape.zone[i])
+}
 
+i <- 10
+FAO56DualCropCalc(modelgrid$cropnames[i], modelgrid$cropcode[i], modelgrid$AD.percentage[i], modelgrid$root.depths[i], modelgrid$irrtype[i], crop.parameters.df, model.scaffold, U2.df, P.df, ETo.df, RHmin.df, results_file = 'new', row_start = 1, RDI.min = modelgrid$RDI.min[i], alfalfa.zone = modelgrid$alfalfa.zone[i], grape.zone = modelgrid$grape.zone[i])
