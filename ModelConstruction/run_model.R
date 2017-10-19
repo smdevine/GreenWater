2b27520637ce3de767c3281d6979f53a90cc9ad3
smdevine <smdevine@ucdavis.edu>
2017-10-19 13:30:19 -0700

    includes code for model re-run begun 10/18/17

--- ModelConstruction/run_model.R
+++ ModelConstruction/run_model.R
@@ -1,13 +1,13 @@
 library(foreach)
 library(doSNOW)
-modelscaffoldDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/model_scaffold/run_model/Sep2017' #location of input data
-setwd(modelscaffoldDir)
-cropscape_legend <- read.csv('cropscape_legend.txt', stringsAsFactors = FALSE)
-alfalfa_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Alfalfa'] #75380 total
-grape_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Grapes']
-almond_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Almonds']
-walnut_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Walnuts']
-pistachio_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Pistachios']
+# modelscaffoldDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/model_scaffold/run_model/Oct2017' #location of input data
+# setwd(modelscaffoldDir)
+# cropscape_legend <- read.csv('cropscape_legend.txt', stringsAsFactors = FALSE)
+# alfalfa_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Alfalfa'] #75380 total
+# grape_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Grapes']
+# almond_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Almonds']
+# walnut_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Walnuts']
+# pistachio_code <- cropscape_legend$VALUE[cropscape_legend$CLASS_NAME=='Pistachios']
 
 #re-run 12 almond scenarios
 #start time 9/1/17 12:34 PM
@@ -115,5 +115,22 @@ foreach(i=1:10) %dopar% {
   FAO56DualCropCalc(modelgrid$cropnames[i], modelgrid$cropcode[i], modelgrid$AD.percentage[i], modelgrid$root.depths[i], modelgrid$irrtype[i], crop.parameters.df, model.scaffold, U2.df, P.df, ETo.df, RHmin.df, results_file = 'new', row_start = 1, RDI.min = modelgrid$RDI.min[i], alfalfa.zone = modelgrid$alfalfa.zone[i], grape.zone = modelgrid$grape.zone[i])
 }
 
-i <- 10
-FAO56DualCropCalc(modelgrid$cropnames[i], modelgrid$cropcode[i], modelgrid$AD.percentage[i], modelgrid$root.depths[i], modelgrid$irrtype[i], crop.parameters.df, model.scaffold, U2.df, P.df, ETo.df, RHmin.df, results_file = 'new', row_start = 1, RDI.min = modelgrid$RDI.min[i], alfalfa.zone = modelgrid$alfalfa.zone[i], grape.zone = modelgrid$grape.zone[i])
+#re-run test on 10/17 due to Ks function call argument error  that should only affect 30 and 80% AD scenarios
+modelgrid <- expand.grid(root.depths = c('1.0m', '2.0m', '3.0m'), AD.percentage = c(30, 50, 80), cropnames = c('walnut.mature', 'almond.mature', 'pistachios', 'grapes.table', 'grapes.wine', 'alfalfa.intermountain', 'alfalfa.CV', 'alfalfa.imperial'))
+modelgrid$root.depths <- as.character(modelgrid$root.depths)
+modelgrid$cropnames <- as.character(modelgrid$cropnames)
+modelgrid$AD.percentage <- as.integer(modelgrid$AD.percentage)
+modelgrid$RDI.min <- ifelse(modelgrid$cropnames=='grapes.wine' &  modelgrid$AD.percentage==30, 0.8, ifelse(modelgrid$cropnames=='grapes.wine' &  modelgrid$AD.percentage==50, 0.5, ifelse(modelgrid$cropnames=='grapes.wine' &  modelgrid$AD.percentage==80, 0.2, NA)))
+modelgrid$cropcode <- ifelse(modelgrid$cropnames=='almond.mature', almond_code, ifelse(modelgrid$cropnames=='walnut.mature', walnut_code, ifelse(modelgrid$cropnames=='pistachios', pistachio_code, ifelse(modelgrid$cropnames=='grapes.table', grape_code, ifelse(modelgrid$cropnames=='grapes.wine', grape_code, ifelse(modelgrid$cropnames=='alfalfa.intermountain' | modelgrid$cropnames=='alfalfa.CV' | modelgrid$cropnames=='alfalfa.imperial', alfalfa_code, print('Done')))))))
+modelgrid$irrtype <- ifelse(modelgrid$cropnames=='almond.mature', 'Microspray, orchards', ifelse(modelgrid$cropnames=='walnut.mature', 'Microspray, orchards', ifelse(modelgrid$cropnames=='pistachios', 'Microspray, orchards', ifelse(modelgrid$cropnames=='grapes.table', 'Drip', ifelse(modelgrid$cropnames=='grapes.wine', 'Drip', ifelse(modelgrid$cropnames=='alfalfa.intermountain' | modelgrid$cropnames=='alfalfa.CV' | modelgrid$cropnames=='alfalfa.imperial', 'Border', print('Done')))))))
+modelgrid$alfalfa.zone <- ifelse(modelgrid$cropnames=='alfalfa.intermountain', 'Intermountain', ifelse(modelgrid$cropnames=='alfalfa.CV', 'Central Valley', ifelse(modelgrid$cropnames=='alfalfa.imperial', 'Imperial Valley', NA)))
+modelgrid$grape.zone <- ifelse(modelgrid$cropnames=='grapes.wine', 'Central California Foothills and Coastal Mountains', ifelse(modelgrid$cropnames=='grapes.table', 'Central California Valley', NA))
+modelgrid$AD.percentage[modelgrid$cropnames=='grapes.wine'] <- 50
+
+modelgrid
+cl <- makeCluster(6, type = 'SOCK') #change the number to your desired number of CPU cores  
+clusterExport(cl, list=c("resultsDir", "rounding_digits", "FAO56DualCropCalc", "crop.parameters.df", "model.scaffold", "U2.df", "P.df", "ETo.df", "RHmin.df", "irrigation.parameters", "cropscape_legend"))
+registerDoSNOW(cl)
+foreach(i=1:72) %dopar% {
+  FAO56DualCropCalc(modelgrid$cropnames[i], modelgrid$cropcode[i], modelgrid$AD.percentage[i], modelgrid$root.depths[i], modelgrid$irrtype[i], crop.parameters.df, model.scaffold, U2.df, P.df, ETo.df, RHmin.df, results_file = 'new', row_start = 1, RDI.min = modelgrid$RDI.min[i], alfalfa.zone = modelgrid$alfalfa.zone[i], grape.zone = modelgrid$grape.zone[i])
+}
