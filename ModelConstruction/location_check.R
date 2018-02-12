dd1044c5e84eb82e457185d1ee7692b06774bf92
smdevine <smdevine@ucdavis.edu>
2018-02-12 10:12:34 -0800

    verified projection of original soil map unit polygons

--- ModelConstruction/location_check.R
+++ ModelConstruction/location_check.R
@@ -1,4 +1,5 @@
 library(raster)
+mainDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion'
 coords <- SpatialPoints(cbind(-2020150, 2221300), proj4string = crs('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')) #this is Albers Equal Area coordinates
 spTransform(coords, "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")
 #result is: -115.653, 33.13325
@@ -15,3 +16,7 @@ crs(test) #this matches AEA definition above
 cropscape <- raster('C:/Users/smdevine/Desktop/Allowable_Depletion/CA_cropscape2015/CDL_2015_06.tif')
 crs(cropscape) #good, I didn't do anything stupid
 projection(cropscape)
+
+#original crs of soil map units
+mapunits <- shapefile(file.path(mainDir, 'soils_data', 'spatial', 'farmland_mapunits_2016.shp'))
+#crs is +proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0 (this is California Teale Albers)
