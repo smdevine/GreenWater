ec743b840860708a9698a1e139e87fc9e40b4d5b
smdevine <smdevine@ucdavis.edu>
2018-03-08 17:01:46 -0800

    cleaned up script for latest SSURGO download

--- GetData/download_SSURGO_allCA.R
+++ GetData/download_SSURGO_allCA.R
@@ -6,41 +6,69 @@
 library(soilDB)
 options(stringsAsFactors = FALSE)
 mainDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion'
-soilsDir <- file.path(mainDir, 'soils_data/soilDB_query/CA_all')
-setwd(soilsDir)
-mu_area <- read.csv('CA_all_mu_data_2017-07-19.csv') #these are the soil mapunits of interest and was produced by this script
-areasymbols.needed <- unique(mu_area$areasymbol) #could have done the SQL style queries by mapunit
+modelscaffoldDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion/model_scaffold/run_model/Mar2018'
+soilsDir <- file.path(mainDir, 'soils_data/soilDB_query/Mar2018') #was CA_all for 2017 download
+AOI <- read.csv(file.path(modelscaffoldDir, "soil.areasymbols.csv")) #these are the soil mapunits of interest and was produced by model_scaffold.v2.R on 3/6/18
+areasymbols.needed <- AOI$areasymbol
 areasymbols.needed <- areasymbols.needed[order(areasymbols.needed)]
+areasymbols.needed <- gsub('ca', 'CA', areasymbols.needed)
 
-query_mapunit <- function() {
-  SDA_query(paste0("SELECT l.areasymbol, mu.mukey, mu.muname, muacres, aws0150wta, aws0100wta, aws050wta 
+query_tab.metadata <- function() {
+  SDA_query(paste0("SELECT areasymbol, tabularversion, tabularverest
+    FROM satabularver
+    WHERE areasymbol LIKE 'CA%'"))
+}
+tab_metadata <- query_tab.metadata()
+head(tab_metadata)
+
+query_spatial.metadata <- function() {
+  SDA_query("SELECT areasymbol, spatialversion, spatialverest
+    FROM saspatialver
+    WHERE areasymbol LIKE 'CA%'") #this is spatial version 2
+}
+spatial_metadata <- query_spatial.metadata()
+head(spatial_metadata)
+write.csv(spatial_metadata[spatial_metadata$areasymbol %in% areasymbols.needed,], file.path(soilsDir, 'spatial_metadata.AOI.csv'), row.names = FALSE)
+
+query_mapunit <- function(x) {
+  SDA_query(paste0("SELECT l.areasymbol, l.tabularversion, mu.mukey, mu.muname, muacres, aws0150wta, aws0100wta, aws050wta 
     FROM legend l
       INNER JOIN mapunit mu ON mu.lkey = l.lkey
         LEFT OUTER JOIN muaggatt muag ON muag.mukey = mu.mukey
-    WHERE l.areasymbol LIKE 'CA%'"))
+    WHERE l.areasymbol = '", x, "'"))
 }
-mu_data <- query_mapunit()
+
+mu_data <- do.call(rbind, lapply(areasymbols.needed, query_mapunit))
+#this was faster
+# query_mapunit <- function(x) {
+#   SDA_query(paste0("SELECT l.areasymbol, l.tabularversion, mu.mukey, mu.muname, muacres, aws0150wta, aws0100wta, aws050wta 
+#     FROM legend l
+#       INNER JOIN mapunit mu ON mu.lkey = l.lkey
+#         LEFT OUTER JOIN muaggatt muag ON muag.mukey = mu.mukey
+#     WHERE l.areasymbol LIKE 'CA%'"))
+# }
+#mu_data <- query_mapunit()
+#mu_data_final <- mu_data_final[mu_data_final$areasymbol %in% areasymbols.needed,]
 dim(mu_data)
 head(mu_data)
-setwd(file.path(soilsDir, 'soilDB_query'))
-write.csv(mu_data, paste0('CA_all_mu_data_', Sys.Date(), '.csv'), row.names = FALSE)
+mu_data_final <- merge(mu_data, tab_metadata, by='areasymbol')
+mu_data_final <- merge(mu_data_final, spatial_metadata, by='areasymbol')
+dim(mu_data_final)
+head(mu_data_final)
+write.csv(mu_data_final, file.path(soilsDir, paste0('CA_all_mu_data_', Sys.Date(), '.csv')), row.names = FALSE)
 
-query_component <- function() {
+query_component <- function(x) {
   SDA_query(paste0("SELECT mu.mukey, comp.cokey, compname, comppct_r, majcompflag, reskind, resdept_r, resdepb_r
     FROM legend l
       INNER JOIN mapunit mu ON mu.lkey = l.lkey
         LEFT OUTER JOIN component comp ON comp.mukey = mu.mukey
           LEFT OUTER JOIN corestrictions cores ON cores.cokey = comp.cokey
-    WHERE l.areasymbol LIKE 'CA%'"))
+    WHERE l.areasymbol = '", x, "'"))
 }
 
-comp_data <- query_component()
-setwd(file.path(soilsDir, 'soilDB_query'))
-write.csv(comp_data, paste0('CA_all_comp_data_', Sys.Date(), '.csv'), row.names = FALSE)
+comp_data <- do.call(rbind, lapply(areasymbols.needed, query_component))
 dim(comp_data)
-length(unique(comp_data$cokey))
-length(unique(comp_data$mukey))
-length(which(is.na(comp_data$cokey)))
+write.csv(comp_data, file.path(soilsDir, paste0('CA_all_comp_data_', Sys.Date(), '.csv')), row.names = FALSE)
 
 query_horizon <- function(x) { #this will not return cokey NAs
   print(x)
@@ -55,9 +83,9 @@ query_horizon <- function(x) { #this will not return cokey NAs
 
 horizon_data <- do.call(rbind, lapply(areasymbols.needed, query_horizon))
 dim(horizon_data)
-length(unique(horizon_data$cokey))
-setwd(file.path(soilsDir))
-write.csv(horizon_data, paste0('CA_all_horizon_data_', Sys.Date(), '.csv'), row.names = FALSE)
+length(unique(horizon_data$cokey)) #75978
+length(unique(comp_data$cokey)) #75979
+write.csv(horizon_data, file.path(soilsDir, paste0('CA_all_horizon_data_', Sys.Date(), '.csv')), row.names = FALSE)
 
 #mapunit level data needed
 #mukey muname aws0150wta aws0100wta aws050wta
