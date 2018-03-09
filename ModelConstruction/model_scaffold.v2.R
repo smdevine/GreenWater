91c6e6d98c4acef73696f5d8111e1d424564cba8
smdevine <smdevine@ucdavis.edu>
2018-03-08 22:59:00 -0800

    incorporated grape zone and alfalfa zone labeling into this script

--- ModelConstruction/model_scaffold.v2.R
+++ ModelConstruction/model_scaffold.v2.R
@@ -1,5 +1,6 @@
 #this is a complete re-working of the model scaffold based on new crops dataset from LandIQ
-#combines code from model_scaffold.R, model_consruction.R, ....
+#combines code from model_scaffold.R, model_consruction.R, getAlfalfaZone.R, getGrapeZone.R into one script
+#still maintains dependency on PRISM, SpatialCIMIS, and SSURGO download scripts, as well as SSURGO data aggregration script
 #generally the same approach except now the fundamental unit of interest is a polygon (or vector in GIS terminology) as opposed to a raster cell, since the crop dataset is a shapefile
 #note that two GIS operations were performed outside of R in ArcMap 10.5 due to rgeos errors
 library(raster)
@@ -13,6 +14,7 @@ SoilMUDir <- file.path(mainDir, 'soils_data/spatial')
 modelscaffoldDir <- file.path(mainDir, 'model_scaffold/run_model/Mar2018')
 soilcomptempDir <- file.path(mainDir, 'model_scaffold/soil_climate_crop/by_componentMar7.2018')
 SoilsDataDir <- file.path(mainDir, 'soils_data/results/spring2018model')
+CropZonesDir <- 'C:/Users/smdevine/Desktop/SpatialData/ecozones/ca_eco_l3'
 
 #cropscape codes for historical consistency with new LandIQ crops dataset
 cropscape_legend <- read.csv('C:/Users/smdevine/Desktop/Allowable_Depletion/CA_cropscape2015/cropscape_legend.txt', stringsAsFactors = FALSE)
@@ -85,8 +87,8 @@ nrow(crops.soils) #now, 323,422 features
 length(unique(crops.soils$mukey)) #only 5,301 mukeys
 crops.soils$Acres <- area(crops.soils)/10000*2.47105
 sum(crops.soils$Acres) #now, 3,674,350 acres, loss of 1345 acres
-crops.soils$crop.soils.code <- paste0(crops.soils$cropcode, crops.soils$mukey)
-length(unique(crops.soils$crop.soils.code)) #9916 unique mapunit x crop combinations
+#crops.soils$crop.soils.code <- paste0(crops.soils$cropcode, crops.soils$mukey)
+#length(unique(crops.soils$crop.soils.code)) #9916 unique mapunit x crop combinations
 max(crops.soils$Acres) #is 580.9 acres; use gCentroids to get centroids
 #crops.soils$FID_combined <- paste0(crops.soils$FID_LandIQ, crops.soils$FID_farmla)
 #length(unique(crops.soils$FID_combined))
@@ -109,7 +111,7 @@ CIMIScellnumbers <- as.integer(cellFromXY(object=spatialCIMIS, xy=centroids.sp.d
 summary(CIMIScellnumbers)
 length(unique(CIMIScellnumbers)) #13,060 CIMIS cells need sampling
 CIMIScellunique_df <- data.frame(CIMIS_cells=unique(CIMIScellnumbers))
-write.csv(CIMIScellunique_df, file.path(modelscaffoldDir, 'CIMIS_cells_unique.csv'), row.names = F)
+#write.csv(CIMIScellunique_df, file.path(modelscaffoldDir, 'CIMIS_cells_unique.csv'), row.names = F)
 
 #get cell numbers from PRISM for each point of interest
 prism <- raster(file.path(PRISMDir, '2003/PRISM_ppt_stable_4kmD2_20031001_bil', 'PRISM_ppt_stable_4kmD2_20031001_bil.bil'))
@@ -118,13 +120,64 @@ centroids.sp.geo.df <- spTransform(centroids.sp.df, crs(prism))
 PRISMcellnumbers <- as.integer(cellFromXY(object = prism, xy = centroids.sp.geo.df))
 length(unique(PRISMcellnumbers)) #4546 PRISM cells need sampling
 PRISMcellunique_df <- data.frame(PRISM_cells=unique(PRISMcellnumbers))
-write.csv(PRISMcellunique_df, file.path(modelscaffoldDir, 'PRISM_cells_unique.csv'), row.names = F)
+#write.csv(PRISMcellunique_df, file.path(modelscaffoldDir, 'PRISM_cells_unique.csv'), row.names = F)
 
 #write unique soil survey area names to file for running the soilDB query in 'download_SSURGO_allCA.R'
 write.csv(data.frame(areasymbol=unique(crops.soils$areasymbol)), file.path(modelscaffoldDir, 'soil.areasymbols.csv'), row.names=FALSE)
 
+#get grape zones for each grape field
+list.files(CropZonesDir)
+grape.zones <- shapefile(file.path(CropZonesDir, 'ca_eco_l3.shp'))
+centroids.sp.aea.df <- spTransform(centroids.sp.df, crs(grape.zones))
+grape.zone.names <- over(centroids.sp.aea.df, grape.zones) #do this for all crop x soil centroids to make merge with crop.soils easier; grape.zone.names changed to NA below if not a grape field 
+dim(grape.zone.names)
+#summary(as.factor(grape.zone.names$US_L3NAME))
+#Cascades 
+# 44 
+#Central California Foothills and Coastal Mountains 
+#69164 
+#Central California Valley 
+#58582 
+#Coast Range 
+#1852 
+#Eastern Cascades Slopes and Foothills 
+#5 
+#Klamath Mountains/California High North Coast Range 
+#601 
+#Mojave Basin and Range 
+#19 
+#Sierra Nevada 
+#1374 
+#Sonoran Basin and Range 
+#1371 
+#Southern California Mountains 
+#174 
+#Southern California/Northern Baja Coast 
+#2381 
+grape.zone.names <- grape.zone.names['US_L3NAME']
+grape.zone.names$grape.zone.name <- ifelse(grape.zone.names$US_L3NAME == 'Cascades' | grape.zone.names$US_L3NAME == 'Central California Foothills and Coastal Mountains' | grape.zone.names$US_L3NAME == 'Coast Range' | grape.zone.names$US_L3NAME == 'Eastern Cascades Slopes and Foothills' | grape.zone.names$US_L3NAME == 'Klamath Mountains/California High North Coast Range' | grape.zone.names$US_L3NAME == 'Sierra Nevada' | grape.zone.names$US_L3NAME == 'Southern California Mountains' | grape.zone.names$US_L3NAME == 'Southern California/Northern Baja Coast', 'Coast, Foothills, and Mountains', ifelse(grape.zone.names$US_L3NAME == 'Central California Valley', 'Central Valley', 'Southern Desert'))
+grape.zone.names$grape.zone.name[centroids.sp.aea.df$cropcode!='69'] <- NA #maintain all ecozone names to add back to crop.soils
+summary(as.factor(grape.zone.names$grape.zone.name))
+summary(as.factor(grape.zone.names$US_L3NAME))
+crops.soils$grape.zone <- grape.zone.names$grape.zone.name
+crops.soils$ecozone_L3NAME <- grape.zone.names$US_L3NAME
+sum(!is.na(crops.soils$grape.zone))
+sum(crops.soils$cropcode=='69')
+
+#add alfalfa zone
+alfalfa.zones <- shapefile(file.path(CropZonesDir, 'alfalfa.zones.shp'))
+centroids.sp.aea.df <- spTransform(centroids.sp.df, crs(alfalfa.zones))
+alfalfa.zone.names <- over(centroids.sp.aea.df, alfalfa.zones)
+alfalfa.zone.names$name[centroids.sp.aea.df$cropcode!='36'] <- NA
+crops.soils$alfalfa.zone <- alfalfa.zone.names$name
+sum(!is.na(crops.soils$alfalfa.zone))
+sum(crops.soils$cropcode=='36')
+sum(crops.soils$Acres[crops.soils$cropcode=='36']) #928,710.5
+sum(crops.soils$Acres[crops.soils$cropcode=='36' & !is.na(crops.soils$alfalfa.zone)]) #874815, so excludes 53,896 or about 5.8% of LandIQ identified alfalfa
+
 #now on to the model_scaffold
 model_scaffold <- crops.soils
+model_scaffold$cropcode <- as.integer(model_scaffold$cropcode)
 lapply(data.frame(model_scaffold), class)
 model_scaffold$PRISMcellnumber <- PRISMcellnumbers
 model_scaffold$CIMIScellnumber <- CIMIScellnumbers
@@ -139,20 +192,21 @@ model_codes_matrix <- data.frame(model_code=model_codes, unique_model_code=uniqu
 model_scaffold_final <- merge(model_scaffold, model_codes_matrix, by='model_code')
 nrow(model_scaffold_final)
 head(model_scaffold_final, 20) #check manually that codes maintained integrity
-model_scaffold_final$crop.soils.code <- NULL
+#model_scaffold_final$crop.soils.code <- NULL
 model_scaffold_final$latitude_NAD83 <- coordinates(centroids.sp.geo.df)[,2]
 model_scaffold_final$longitude_NAD83 <- coordinates(centroids.sp.geo.df)[,1]
 shapefile(model_scaffold_final, file.path(modelscaffoldDir, 'shapefiles', 'model_scaffold.3.6.18.shp'), overwrite=TRUE) #still in same projection as original LandIQ shapefile 'i15_Crop_Mapping_2014_Final_LandIQonAtlas.shp'; field_names abbreviated due to shapefile contraints
+write.csv(data.frame(model_scaffold_final), file.path(modelscaffoldDir, 'model_scaffold_allfields.3.18.18.csv'), row.names = FALSE) #this is a full copy of the model scaffold that contains duplicate unique climate x crop x map unit combinations
 
 #create model_scaffold for the only the unique model codes and write to disk as csv
 #maintain this order in columns: (1)mukey	(2)crop_code	(3)longitude_AEA	(4)latitude_AEA	(5)PRISMcellnumber	(6)CIMIScellnumber	(7)unique_model_code
 
 unique_indices <- match(unique_model_codes, model_scaffold_final$unique_model_code)
-model_scaffold_unique <- data.frame(model_scaffold_final[unique_indices, c('mukey', 'cropcode', 'longitude_NAD83', 'latitude_NAD83', 'PRISMcellnumber', 'CIMIScellnumber', 'unique_model_code')])
+model_scaffold_unique <- data.frame(model_scaffold_final[unique_indices, c('mukey', 'cropcode', 'longitude_NAD83', 'latitude_NAD83', 'PRISMcellnumber', 'CIMIScellnumber', 'unique_model_code', 'alfalfa.zone', 'grape.zone', 'ecozone_L3NAME')])
 head(model_scaffold_unique)
-write.csv(model_scaffold_unique, file.path(modelscaffoldDir, 'model_scaffold_codes3.6.18.csv'), row.names = FALSE) #this destroys the model code numbers when read back in, so they were deleted to avoid future confusion. they can simply be re-created by paste0(model_scaffold_unique$mukey, model_scaffold_unique$crop_code, model_scaffold_unique$PRISMcellnumber, model_scaffold_unique$CIMIScellnumber) in this order
-model_scaffold <- read.csv(file.path(modelscaffoldDir, 'model_scaffold_codes3.6.18.csv'), stringsAsFactors = FALSE)
-
+write.csv(model_scaffold_unique, file.path(modelscaffoldDir, 'model_scaffold_codes3.8.18.csv'), row.names = FALSE) #this destroys the model code numbers when read back in, so they were deleted to avoid future confusion. they can simply be re-created by paste0(model_scaffold_unique$mukey, model_scaffold_unique$crop_code, model_scaffold_unique$PRISMcellnumber, model_scaffold_unique$CIMIScellnumber) in this order
+model_scaffold <- read.csv(file.path(modelscaffoldDir, 'model_scaffold_codes3.8.18.csv'), stringsAsFactors = FALSE)
+dim(model_scaffold)
 #head(model_scaffold)
 #dim(model_scaffold) #242,714 rows
 #lapply(model_scaffold, class)
@@ -201,25 +255,19 @@ for (i in seq_len(maxcomps)) {
 fnames <- list.files(path=soilcomptempDir, pattern = glob2rx('*csv'), full.names = TRUE)
 master.file <- do.call(rbind, lapply(fnames, read.csv))
 dim(master.file)
-#1,177,027 unique soil components, climate, and crop for original raster based approach; 1,681,860 with additional crops added 9/12/18); now with vector based LandIQ 5 perennial crops layer: 476,007 unique combinations (includes minor components)
+#1,177,027 unique soil components, climate, and crop for original raster based approach; 1,681,860 with additional crops added 9/12/18); now with vector based LandIQ 5 perennial crops layer: 476,007 unique combinations (this total includes minor components)
 sum(master.file$majcompflag=='Yes') #112,136 major component only
 sum(master.file$comppct_r>=15, na.rm = TRUE) #116,251 >15% component only
-j <- which(master.file$majcompflag=='Yes' & master.file$SSURGO_awc_data=='Yes')
-length(j) #112,136 are major components though 116,251 are >= 15% of mapunits (verified 3/7/18); of these, 109,298 originally had data
+sum(master.file$majcompflag=='Yes' & master.file$SSURGO_awc_data=='Yes') #112,136 are major components though 116,251 are >= 15% of mapunits (verified 3/7/18); of these, 109,298 originally had data
 summary(as.factor(master.file$majcompflag)) #confirmed no NAs
 summary(master.file$comppct_r[master.file$majcompflag=='Yes'])
 hist(master.file$comppct_r[master.file$majcompflag=='Yes'])
-#left off here 3/7/18 5:27 pm
-model_scaffold_majcomps <- master.file[which(master.file$comppct_r >= 15), ]
+model_scaffold_majcomps <- master.file[master.file$majcompflag=='Yes', ]
 dim(model_scaffold_majcomps)
+#left off here 3/8/18 5:07 pm
 
 #add alfalfa zones to model scaffold
-setwd(file.path(model_scaffoldDir, 'run_model/Sep2017'))
-alfalfa_zones_codes <- read.csv('alfalfa_zones_codes.csv', stringsAsFactors = FALSE)
-model_scaffold_majcomps$alfalfa.zone <- alfalfa_zones_codes$zone[match(model_scaffold_majcomps$unique_model_code, alfalfa_zones_codes$unique_model_code)]
-grape_zones_codes <- read.csv('grape_zones_codes.csv', stringsAsFactors = FALSE)
-model_scaffold_majcomps$grape.zone <- grape_zones_codes$grape.zone[match(model_scaffold_majcomps$unique_model_code, grape_zones_codes$unique_model_code)]
-write.csv(model_scaffold_majcomps, file.path(model_scaffoldDir, 'model_scaffold_majcomps.v3.csv'), row.names = F) #v1 included alfalfa.zone column; v2 includes both alfalfa and grape zone column; v3 also includes column identifying whether or not column originally had data
+write.csv(model_scaffold_majcomps, file.path(modelscaffoldDir, 'model_scaffold_majcomps.v3.csv'), row.names = F) #v1 included alfalfa.zone column; v2 includes both alfalfa and grape zone column; v3 also includes column identifying whether or not column originally had data
 
 #investigate NAs and 0's
 mukey_AD_isNA <- unique(model_scaffold2$mukey[which(is.na(model_scaffold2$allowable_depletion))])
