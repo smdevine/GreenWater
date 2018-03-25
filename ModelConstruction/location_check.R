32e9995417e1dfd43449887d219470fa316586e3
smdevine <smdevine@ucdavis.edu>
2018-03-25 11:35:57 -0700

    no significant change here

--- ModelConstruction/location_check.R
+++ ModelConstruction/location_check.R
@@ -1,6 +1,6 @@
 library(raster)
 mainDir <- 'C:/Users/smdevine/Desktop/Allowable_Depletion'
-coords <- SpatialPoints(cbind(-2020150, 2221300), proj4string = crs('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')) #this is Albers Equal Area coordinates
+coords <- SpatialPoints(cbind(-2045640, 1740060), proj4string = crs('+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs')) #this is Albers Equal Area coordinates
 spTransform(coords, "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")
 #result is: -115.653, 33.13325
 # -1809120	1305900
