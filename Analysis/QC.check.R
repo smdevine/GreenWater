1433f08c4ccab7dda3a7b61d59a5f68906e55708
smdevine <smdevine@ucdavis.edu>
2018-04-06 16:22:01 -0700

    added section to check wine grape run results

--- Analysis/QC.check.R
+++ Analysis/QC.check.R
@@ -11,11 +11,36 @@ almond.results$GW.ET.growing[almond.results$unique_model_code==code]
 almond.results.aggregated$GW.ET.growing[almond.results.aggregated$unique_model_code==code]
 colnames(almond.results.aggregated)
 
+#initialize directory names then loop through
 cropnames <- c('almond.mature', 'walnut.mature', 'pistachios', 'grapes.table', 'alfalfa.intermountain', 'alfalfa.CV', 'alfalfa.imperial')
+cropnames <- paste0(cropnames, '_majcomps')
 #go through the complete results and run a QC check
 for(j in seq_along(cropnames)) {
-  cropnames <- c('almond.mature', 'walnut.mature', 'pistachios', 'grapes.table', 'alfalfa.intermountain', 'alfalfa.CV', 'alfalfa.imperial')
-  cropnames <- paste0(cropnames, '_majcomps')
+  scenario_names <- list.dirs(file.path('C:/Users/smdevine/Desktop/Allowable_Depletion/results/Dec2017.check', cropnames[j]), full.names = FALSE, recursive = FALSE)
+  for(k in seq_along(scenario_names)) {
+    fnames <- list.files(file.path('C:/Users/smdevine/Desktop/Allowable_Depletion/results/Dec2017.check', cropnames[j], scenario_names[k]), full.names = TRUE)
+    fnames_simple <- list.files(file.path('C:/Users/smdevine/Desktop/Allowable_Depletion/results/Dec2017.check', cropnames[j], scenario_names[k]), full.names = FALSE)
+    fnames <- fnames[1:(length(fnames)-2)] #last two files in each directory are overall results and metadata files
+    results <- as.data.frame(matrix(data=NA, nrow=length(fnames), ncol=7))
+    colnames(results) <- c('filename','wb', 'rel.error', 'neg.Kei.count', 'neg.Ks.count', 'neg.Ir.count', 'neg.ET.count')
+    for(i in seq_along(fnames)) {
+      df <- read.csv(fnames[i], stringsAsFactors = FALSE)
+      results$filename[i] <- fnames_simple[i]
+      results$wb[i] <- sum(df$P) + sum(df$Ir[1:(nrow(df) - 1)]) - sum(df$ETc.act) - sum(df$DPr) + (df$Dr.end[nrow(df)] - df$Dr.initial[1])
+      results$rel.error[i] <- 100 * (results$wb[i] / sum(df$P, df$Ir[1:(nrow(df) - 1)]))
+      results$neg.Kei.count[i] <- sum(df$Kei < 0)
+      results$neg.Ks.count[i] <- sum(df$Ks < 0)
+      results$neg.Ir.count[i] <- sum(df$Ir < 0)
+      results$neg.ET.count[i] <- sum(df$ETc.act < 0)
+      write.csv(results, file =  file.path('C:/Users/smdevine/Desktop/Allowable_Depletion/results/Dec2017.check/QC.results', paste0('QCcheck_', cropnames[j], '_', scenario_names[k],  Sys.Date(), '.csv')))
+    }
+  }
+}
+
+#and now for grapes.wine
+cropnames <- c('grapes.wine')
+cropnames <- paste0(cropnames, '_majcomps')
+for(j in seq_along(cropnames)) {
   scenario_names <- list.dirs(file.path('C:/Users/smdevine/Desktop/Allowable_Depletion/results/Dec2017.check', cropnames[j]), full.names = FALSE, recursive = FALSE)
   for(k in seq_along(scenario_names)) {
     fnames <- list.files(file.path('C:/Users/smdevine/Desktop/Allowable_Depletion/results/Dec2017.check', cropnames[j], scenario_names[k]), full.names = TRUE)
